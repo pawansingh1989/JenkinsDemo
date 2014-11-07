2a795950841b4e29ea5f9e60eef030885df4c1a5
Pawan Singh <pawan.singh@gspann.com>
2014-11-07 13:20:19 +0530

    jenkins

---
+++ src/test/java/org/jenkins/JenkinsTest.java
@@ -0,0 +1,12 @@
+package org.jenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+
+	
+	@Test
+	public void testJenkins(){
+		System.out.println("############### Successful Test");
+	}
+}
